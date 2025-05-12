914a155ed5760cfd0c3a7e9c1ef927892c3b87c1
Christoph Kappestein <christoph.kappestein@gmail.com>
2025-05-12 21:08:47 +0200

    add actions

--- src/app/components/project/detail/detail.component.ts
+++ src/app/components/project/detail/detail.component.ts
@@ -3,6 +3,8 @@ import {Detail, ErrorService} from "ngx-fusio-sdk";
 import {ActivatedRoute, Router} from "@angular/router";
 import {Project} from "../../../generated/Project";
 import {ProjectService} from "../../../services/project.service";
+import {ApiService} from "../../../api.service";
+import {Message} from "../../../generated/Message";
 
 @Component({
   selector: 'app-project-detail',
@@ -11,12 +13,34 @@ import {ProjectService} from "../../../services/project.service";
 })
 export class DetailComponent extends Detail<Project> {
 
-  constructor(private service: ProjectService, route: ActivatedRoute, router: Router, error: ErrorService) {
+  result?: Message;
+
+  constructor(private service: ProjectService, private api: ApiService, route: ActivatedRoute, router: Router, error: ErrorService) {
     super(route, router, error);
   }
 
   protected getService(): ProjectService {
     return this.service;
+  }
+
+  async doCertbot(id: any) {
+    this.result = await this.api.getClient().project().certbot('' + id, {});
+  }
+
+  async doPull(id: any) {
+    this.result = await this.api.getClient().project().pull('' + id, {});
+  }
+
+  async doUp(id: any) {
+    this.result = await this.api.getClient().project().up('' + id, {});
+  }
+
+  async doDown(id: any) {
+    this.result = await this.api.getClient().project().down('' + id, {});
+  }
+
+  async doLogs(id: any) {
+    this.result = await this.api.getClient().project().logs('' + id);
   }
 
 }

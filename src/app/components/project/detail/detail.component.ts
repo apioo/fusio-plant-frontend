f272f76761836b53847f3b88fcc48850b19136de
Christoph Kappestein <christoph.kappestein@gmail.com>
2025-05-26 00:01:45 +0200

    update details

--- src/app/components/project/detail/detail.component.ts
+++ src/app/components/project/detail/detail.component.ts
@@ -5,6 +5,8 @@ import {Project} from "../../../generated/Project";
 import {ProjectService} from "../../../services/project.service";
 import {ApiService} from "../../../api.service";
 import {Message} from "../../../generated/Message";
+import {DockerStatistics} from "../../../generated/DockerStatistics";
+import {DockerProcesses} from "../../../generated/DockerProcesses";
 
 @Component({
   selector: 'app-project-detail',
@@ -14,6 +16,8 @@ import {Message} from "../../../generated/Message";
 export class DetailComponent extends Detail<Project> {
 
   result?: Message;
+  statistics?: DockerStatistics;
+  processes?: DockerProcesses;
   loading = false;
 
   constructor(private service: ProjectService, private api: ApiService, route: ActivatedRoute, router: Router, error: ErrorService) {
@@ -24,40 +28,10 @@ export class DetailComponent extends Detail<Project> {
     return this.service;
   }
 
-  async doCertbot(id: any) {
-    this.loading = true;
-    try {
-      this.result = await this.api.getClient().project().certbot('' + id, {});
-    } catch (error) {
-      this.result = this.error.convert(error);
-    }
-    this.loading = false;
-  }
-
-  async doPull(id: any) {
-    this.loading = true;
-    try {
-      this.result = await this.api.getClient().project().pull('' + id, {});
-    } catch (error) {
-      this.result = this.error.convert(error);
-    }
-    this.loading = false;
-  }
-
-  async doUp(id: any) {
-    this.loading = true;
-    try {
-      this.result = await this.api.getClient().project().up('' + id, {});
-    } catch (error) {
-      this.result = this.error.convert(error);
-    }
-    this.loading = false;
-  }
-
   async doDown(id: any) {
     this.loading = true;
     try {
-      this.result = await this.api.getClient().project().down('' + id, {});
+      this.result = await this.api.getClient().project().execute().down('' + id, {});
     } catch (error) {
       this.result = this.error.convert(error);
     }
@@ -67,7 +41,47 @@ export class DetailComponent extends Detail<Project> {
   async doLogs(id: any) {
     this.loading = true;
     try {
-      this.result = await this.api.getClient().project().logs('' + id);
+      this.result = await this.api.getClient().project().execute().logs('' + id);
+    } catch (error) {
+      this.result = this.error.convert(error);
+    }
+    this.loading = false;
+  }
+
+  async doPs(id: any) {
+    this.loading = true;
+    try {
+      this.processes = await this.api.getClient().project().execute().ps('' + id, {});
+    } catch (error) {
+      this.result = this.error.convert(error);
+    }
+    this.loading = false;
+  }
+
+  async doPull(id: any) {
+    this.loading = true;
+    try {
+      this.result = await this.api.getClient().project().execute().pull('' + id, {});
+    } catch (error) {
+      this.result = this.error.convert(error);
+    }
+    this.loading = false;
+  }
+
+  async doStats(id: any) {
+    this.loading = true;
+    try {
+      this.statistics = await this.api.getClient().project().execute().stats('' + id, {});
+    } catch (error) {
+      this.result = this.error.convert(error);
+    }
+    this.loading = false;
+  }
+
+  async doUp(id: any) {
+    this.loading = true;
+    try {
+      this.result = await this.api.getClient().project().execute().up('' + id, {});
     } catch (error) {
       this.result = this.error.convert(error);
     }
